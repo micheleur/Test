42c5902a566eac8df716756bd247aa10e14bd131
Michele Urietti <michele.urietti@locomad.it>
2014-04-28 17:57:22 +0200

    aggiunta form inserzione nome.

--- file.php
+++ file.php
@@ -2,10 +2,16 @@
 
 //test php
 function Saluti($nome){
-	return "Ciao ". $nome ." !";
+//aggiungo una formattazione
+	return "Ciao <strong>". $nome ."</strong> !";
 }
 
  echo Saluti("Poldo");
 
 //ciao nginx
 ?>
+<form>
+ <label>Nome:</label>
+ <input type="text" name="nome">
+ <input type="submit" value="INVIA">
+</form>
