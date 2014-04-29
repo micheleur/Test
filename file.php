495f097ab4a09d1f8366bd6f30ee6c5fb65a09f4
Michele Urietti <michele.urietti@locomad.it>
2014-04-29 11:34:56 +0200

    Revert "salvo modifiche al file.php"
    This reverts commit 09cacd22977c9bab415060edba0a92628ca2f3ea.
    
    Conflicts:
    
    	file.php

--- file.php
+++ file.php
@@ -8,8 +8,12 @@ function Saluti($nome){
  echo Saluti("Poldo");
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 //ciao nginx modifico...qui
+=======
+//ciao
+>>>>>>> parent of 09cacd2... salvo modifiche al file.php
 =======
 //ciao
 >>>>>>> parent of 09cacd2... salvo modifiche al file.php
